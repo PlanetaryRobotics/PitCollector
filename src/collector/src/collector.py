ac447fda4af5aeff55d185bd0f261f890ed8847f
jsford <jsford@andrew.cmu.edu>
2019-10-01 13:06:38 -0400

    Fix Pan/Tilt Unit by updating udev rules and removing conflict with xsens.

--- src/collector/src/collector.py
+++ src/collector/src/collector.py
@@ -106,34 +106,71 @@ def demo_relays():
             writeDIO(output_pins[i], state)
         rate.sleep()
 
-def demo_ptu():
-    rospy.init_node('collector')
-    rate = rospy.Rate(10)
-
-    ptu = PanTilt()
-    while not rospy.is_shutdown():
-        ptu = PanTilt()
-        ptu.setPanTilt(-90, -45)
-        ptu.setPanTilt(-60, -45)
-        ptu.setPanTilt(-30, -45)
-        ptu.setPanTilt(-0, -45)
-        ptu.setPanTilt(30, -45)
-        ptu.setPanTilt(60, -45)
-        ptu.setPanTilt(90, -45)
-        ptu.setPanTilt(0, 0)
-        rate.sleep()
 
 from sensor_msgs.msg import Imu
-
+from std_msgs.msg import Float32
 import numpy as np
+imuCounter=0
+xAve = 0
+yAve = 0
+zAve = 0
+wAve = 0
+
+#average 80 measurements coming from the IMU quaternion
+#convert those average to euler degrees
+#publish those values to a topic
 def imuCallback(data):
+    global imuCounter
+    global xAve
+    global yAve
+    global zAve
+    global wAve
+
     quat = data.orientation
-    roll,pitch,yaw = euler_from_quaternion([quat.x,quat.y,quat.z,quat.w])
-    roll = np.degrees(roll)
-    pitch = np.degrees(pitch)
-    yaw = np.degrees(yaw)
+
+    #we are averaging 80 measurements to reduce noise in the IMU readings
+    #inspiration comes from radpiper/robot_controller_node.cpp
+    if (imuCounter%80 != 0):
+        xAve += quat.x;
+        yAve += quat.y;
+        zAve += quat.z;
+        wAve += quat.w;
+    else:
+        x = xAve / 80;
+        y = yAve / 80;
+        z = zAve / 80;
+        roll,pitch,yaw = euler_from_quaternion([quat.x,quat.y,quat.z,quat.w])
+
+        roll = np.degrees(roll)
+        pitch = np.degrees(pitch)
+        yaw = np.degrees(yaw)
+
+        roll_pub = rospy.Publisher('/imu/roll', Float32, queue_size=500)
+        pitch_pub = rospy.Publisher('/imu/pitch', Float32, queue_size=500)
+        yaw_pub = rospy.Publisher('/imu/yaw', Float32, queue_size=500)
+
+        roll_msg = Float32()
+        pitch_msg = Float32()
+        yaw_msg = Float32()
+
+        roll_msg.data = roll
+        pitch_msg.data = pitch
+        yaw_msg.data = yaw
+
+        roll_pub.publish(pitch_msg)
+        pitch_pub.publish(roll_msg)
+        yaw_pub.publish(yaw_msg)
+
+        print('roll: %s pitch %s yaw %s' % (roll, pitch, yaw))
+
+        #reset the averages back to 0 for every 80 cycles
+        xAve = 0
+        yAve = 0
+        zAve = 0
+        wAve = 0
+
+    imuCounter +=1
     #print(quat) 
-    print('roll: %s pitch %s yaw %s' % (roll, pitch, yaw))
 
 
 def demo_imu():
@@ -141,9 +178,22 @@ def demo_imu():
     rospy.Subscriber("/imu/data", Imu, imuCallback)
     rospy.spin()
 
+def demo_ptu():
+    rospy.init_node('collector')
+
+    setPanTilt(-90, -45)
+    setPanTilt(-60, -45)
+    setPanTilt(-30, -45)
+    setPanTilt(0, -45)
+    setPanTilt(30, -45)
+    setPanTilt(60, -45)
+    setPanTilt(90, -45)
+    setPanTilt(0, 0)
+
 
 if __name__=="__main__":
     #main()
     #demo_relays()
     demo_ptu()
     #demo_imu()
+
--- src/collector/src/pan_tilt_helpers.py
+++ src/collector/src/pan_tilt_helpers.py
@@ -1,54 +1,17 @@
-import serial
+import rospy
+import std_msgs.msg
+from pan_tilt_driver.srv import *
 import time
 
-class PanTilt:
-    PR = 92.571429
-    TR = 46.285714
+def setPanTilt(pan_deg, tilt_deg):
+    rospy.wait_for_service('ptu/goto')
+    try:
+        ptu_srv = rospy.ServiceProxy('ptu/goto', ptugoto)
+        req = ptugotoRequest()
+        req.pan = int(pan_deg)
+        req.tilt = int(tilt_deg)
+        resp = ptu_srv(req)
+    except rospy.ServiceException, e:
+        print("Service call failed: {}".format(e))
 
-    def __init__(self, port="/dev/ptu"):
-        self.ser = serial.Serial(port, 9600, timeout=5)
-        if not self.ser.is_open:
-            raise("Failed to open Pan/Tilt serial port.")
-        self.setPanTilt(0, 0)
-
-    def setPanTilt(self, pan_deg, tilt_deg, delay=5.0):
-        self.setPan(pan_deg, 0.0)
-        self.setTilt(tilt_deg, 0.0)
-        time.sleep(delay)
-
-    def setPan(self, pan_deg, delay=5.0):
-        pan_ticks = self.deg2PanTicks(pan_deg)
-        pan_str = 'PP'+str(int(pan_ticks))+' \n'
-        b = bytearray()
-        b.extend(pan_str)
-        print(b)
-        self.ser.write(b)
-        self.ser.write(b)
-        self.ser.write(b)
-        self.ser.write(b)
-        time.sleep(delay)
-
-    def setTilt(self, tilt_deg, delay=5.0):
-        tilt_ticks = self.deg2TiltTicks(tilt_deg)
-        tilt_str = 'TP'+str(int(tilt_ticks))+' \n'
-        b = bytearray()
-        b.extend(tilt_str)
-        print(b)
-        self.ser.write(b)
-        self.ser.write(b)
-        self.ser.write(b)
-        self.ser.write(b)
-        time.sleep(delay)
-
-    def deg2PanTicks(self, pan_deg):
-        return pan_deg / (PanTilt.PR / 3600)
-
-    def deg2TiltTicks(self, tilt_deg):
-        return tilt_deg / (PanTilt.TR / 3600)
-
-if __name__=="__main__":
-    ptu = PanTilt()
-    ptu.setPanTilt(-20, 30)
-    ptu.setPanTilt(0, 0)
-    ptu.setPanTilt(20, -30)
-    ptu.setPanTilt(0, 0)
+    time.sleep(5)
